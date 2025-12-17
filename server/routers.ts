df89711bf9ce7af6b48b1b4af47059212edcec33
BigCorps <interpag.bigcorps@gmail.com>
2025-12-17 16:21:30 +0000

    fix: remove references to non-existent oauth and systemRouter

--- server/routers.ts
+++ server/routers.ts
@@ -1,4 +1,3 @@
-import { systemRouter } from "./_core/systemRouter";
 import { router } from "./_core/trpc";
 import { authRouter } from "./routers/auth";
 import { profileRouter } from "./routers/profile";
@@ -8,7 +7,6 @@ import { familyRouter } from "./routers/family";
 
 export const appRouter = router({
   // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
-  system: systemRouter,
   auth: authRouter,
   profile: profileRouter,
   gasStation: gasStationRouter,
